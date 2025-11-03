1a63037e9431644ed9f9a0553aa599e59e201ce0
Dylan Fashbaugh <dylan.fashbaugh@gmail.com>
2025-11-03 09:47:52 -0500

    Testing for MQTT for API

--- PythonUtils/startReceiveTester.sh
+++ PythonUtils/startReceiveTester.sh
@@ -1,6 +1,6 @@
 python MQTTReceiveTester.py \
   --server=b-f3e16066-c8b5-48a8-81b0-bc3347afef80-1.mq.us-east-1.amazonaws.com \
   --port=8883 \
-  --topic=/test \
+  --topic=/lavli/TestMachine1/wash \
   --username=admin \
   --password=lavlidevbroker!321
